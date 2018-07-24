b8569f0d98cae80054af3ca409e02fabf2895411
tateshi <yarumaya0822@gmail.com>
2018-07-24 15:54:37 +0900

    Initial Commit

---
+++ FlipTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlipTest
+//
+//  Created by 八重尾 立志 on 2018/07/24.
+//  Copyright © 2018年 Yaeo Tateshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
